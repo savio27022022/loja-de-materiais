b336c0453ef8c8e48ee19f2eab360794df1d31fa
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2024-12-09 12:05:05 -0300

    Add files via upload

---
+++ modelos/fisico/testar_function.sql
@@ -0,0 +1,5 @@
+-- Utilizar o banco
+USE LojaDeMateriais;
+
+-- Exemplo de uso
+SELECT produtos_disponiveis_na_categoria(3) AS qtd_produtos_disponiveis;
---
+++ modelos/fisico/testar_view.sql
@@ -0,0 +1,5 @@
+-- Utilizar o banco
+USE LojaDeMateriais;
+
+-- Exemplo de uso
+SELECT * FROM produtos_com_categorias_fornecedores;
